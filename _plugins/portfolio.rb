be4494893367f1392542a3db2aebb31d7bd4f436
Geoff Shannon <earthlingzephyr@gmail.com>
2013-05-19 20:20:59 -0700

    Add class for generating the main portfolio page

--- _plugins/portfolio.rb
+++ _plugins/portfolio.rb
@@ -1,6 +1,20 @@
 
 module Jekyll
 
+  class PortfolioMainPage < Page
+    def initialize(site, base, dir, sections)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, "_layouts"), "default.html")
+      self.data['title'] = "Portfolio"
+      self.data['content'] = "monkeysticks"
+    end
+
+  end
+
   class PortfolioPage < Page
     def initialize(site, base, dir, layout, section)
       @site = site
@@ -11,6 +25,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, "_layouts"), "#{layout}.html")
       self.data['title'] = section.capitalize
+      self.data['portfolio'] = "true"
     end
   end
 
@@ -28,13 +43,17 @@ module Jekyll
 
       dir = site.config['portfolio_dir'] || 'portfolio'
 
+      portfolio_pages = []
       Dir.open(dir).each do |section|
         srcdir = File.join(dir,section)
         next unless (File.directory?(srcdir) &&
                      '.' != section && '..' != section)
+        portfolio_pages << section
         site.pages << PortfolioPage.new(site, site.source,
                                         srcdir, layout, section)
       end
+      site.pages << PortfolioMainPage.new(site, site.source,
+                                          dir, portfolio_pages)
     end
   end
 end
