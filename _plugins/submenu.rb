386c9569ae96de632faa0146ff09533d5d0c70c6
Geoff Shannon <earthlingzephyr@gmail.com>
2013-05-20 00:54:01 -0700

    Add skeleton for submenu tag

---
+++ _plugins/submenu.rb
@@ -0,0 +1,15 @@
+module Jekyll
+  class SubmenuTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(context)
+      "#{@text} #{Time.now}"
+    end
+  end
+end
+
+Liquid::Template.register_tag('submenu', Jekyll::SubmenuTag)
