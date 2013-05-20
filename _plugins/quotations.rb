41f16eba148317998bc32cc76ed4ff077cd8446f
Geoff Shannon <earthlingzephyr@gmail.com>
2013-05-20 16:41:54 -0700

    Implement testimonial initialization

--- _plugins/quotations.rb
+++ _plugins/quotations.rb
@@ -1,14 +1,35 @@
 module Jekyll
   class Quotation < Liquid::Tag
+
+    @@quotelist = nil
+    @@randomquotelist = nil
     def initialize(tag_name, text, tokens)
       super
-      @text = text
     end
 
     def render(context)
+      site = context.registers[:site]
+      init_testimonials(site) unless @@quotelist
       "<div id=\"quote\"><blockquote>&ldquo;" +
-        randomquote(context.registers[:site]) +
+        randomquote(site) +
         "&rdquo;</blockquote></div>"
+    end
+
+    def init_testimonials(site)
+      @@quotelist = []
+      @@randomquotelist = []
+      tdir = File.join(site.config["source"], "_testimonials")
+      files = Dir::entries(tdir).select do |entry|
+        (!File.directory?(File.join(tdir,entry)) && !entry.end_with?("~"))
+      end
+
+      files.each do |fname|
+        File.open(File.join(tdir, fname)) do |file|
+          content = file.read.chomp
+          @@quotelist << [fname, content]
+          @@randomquotelist << content
+        end
+      end
     end
 
     def randomquote(site)
