a88bdacd90be77edc3dd0919d144440020df18b8
Geoff Shannon <earthlingzephyr@gmail.com>
2013-05-20 15:18:13 -0700

    In progress but somewhat working submenu

--- _plugins/submenu.rb
+++ _plugins/submenu.rb
@@ -7,7 +7,28 @@ module Jekyll
     end
 
     def render(context)
-      "#{@text} #{Time.now}"
+      site = context.registers[:site]
+      base = site.config["source"]
+
+      filename = context.environments[0]["page"]["path"]
+      # Prepend a slash for ease of comparison with Page.dir
+      subpath = "/"+File.dirname(filename)
+
+      submenu = ""
+      if (subpath != "/.")
+
+        # Filter pages by whether they're in the same subdirectory
+        pages = site.pages.select do |item|
+          (item.dir == subpath)
+        end
+        p pages[0].data
+        # Need to pull out the index.html page first
+        pages.each do |page|
+          # For each page, pull out it's title, and use it in the submenu
+          subpath << "#{page.data['title']}"
+        end
+      end
+      return submenu
     end
   end
 end
